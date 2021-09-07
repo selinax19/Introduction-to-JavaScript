8db36bd436e0f55fe5a44c424a3e3c6dcced2a3b
selina-liu <selinax19@gmail.com>
2021-09-07 16:40:07 -0400

    answered first question

--- index.js
+++ index.js
@@ -19,9 +19,14 @@ Do the following:
 
    HINT: no function required
 */
+const votingAge= 18;
 
-
-
+if(votingAge>= 18){
+  console.log(true);
+}else{
+  console.log(false);
+}
+}
 /*
 Task 1b - Values
 
