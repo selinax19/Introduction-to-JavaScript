4da084c5e9ab7476ca10e9cb6fd78a64919478bc
selina-liu <selinax19@gmail.com>
2021-09-07 18:15:16 -0400

    finished project

--- index.js
+++ index.js
@@ -244,9 +244,14 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
-}
+function annoyingSong(number){
+      for(let i = number; i > 0; i --){
+        return '${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-1} bottles of soda on the wall';
+      }
+
+      }
+      console.log('task 6', annoyingSong(8));
+      console.log('task 6', annoyingSong(7))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -264,9 +269,21 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(number){
+if(number >= 90){
+  return 'you got an A';
+}else if(number < 90 || number >= 80){
+  return 'you got a B';
+}else if(number < 80 && number >= 70){
+  return 'you got a C';
+}else if(number < 70 && number >= 60){
+  return 'you got a D';
+}else if(number < 60){
+  return 'you got an F';
 }
+}
+
+console.log('task 7', grade(85));
 
 
 
